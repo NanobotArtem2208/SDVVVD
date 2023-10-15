7c5b5e4ae020ad31f7ede6baf4a4692676e48013
НаноботАртем2208 <astramoda30@gmail.com>
2023-10-15 21:15:31 +0400

    сайт экскурсий

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-document.addEventListener('mousemove', e => {
-	Object.assign(document.documentElement, {
-		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
-		`
-	})
-})
+document.addEventListener('mousemove', function(e) {
+	var moveX = (e.clientX - window.innerWidth / 2) * -0.005; // Изменение оси X
+	var moveY = (e.clientY - window.innerHeight / 2) * 0.01; // Изменение оси Y
+  
+	document.documentElement.style.setProperty('--move-x', moveX + 'deg');
+	document.documentElement.style.setProperty('--move-y', moveY + 'deg');
+  });
